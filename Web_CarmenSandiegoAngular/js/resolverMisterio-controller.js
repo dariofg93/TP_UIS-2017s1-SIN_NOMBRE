c7bb37dd1ada6e46339832d363091119c997f926
Nico <nicoruiz182@gmail.com>
2017-05-22 16:56:52 -0300

    NR - Se agregaron cosas a Resolver misterio

--- Web_CarmenSandiegoAngular/js/resolverMisterio-controller.js
+++ Web_CarmenSandiegoAngular/js/resolverMisterio-controller.js
@@ -1,8 +1,43 @@
+$(document).ready(function(){
+    //Components initialization
+    $('.modal').modal();
+    $('select').material_select();
+});
+
 angular.module("CarmenSandiego")
 
 .controller("ResolverMisterioController", function() {
     this.title = "Resolver Misterio";
-
+    this.paisSeleccionado = "";
+    this.lugarSeleccionado;
+    this.pista; 
+    this.villanoSeleccionado = "";
+    this.villanos = [
+        {
+            "nombre": "Carmen San Diego",
+            "id": 1
+        },
+        {
+            "nombre": "Higor Hiorovich",
+            "id": 2
+        },
+        {
+            "nombre": "Elvis Presley",
+            "id": 3
+        },
+        {
+            "nombre": "Al Capone",
+            "id": 4
+        },
+        {
+            "nombre": "Lucei Lou",
+            "id": 5
+        },
+        {
+            "nombre": "Betty Chiars",
+            "id": 6
+        }
+    ]
     this.caso = {
         "id": 1,
         "pais": {
@@ -33,6 +68,22 @@ angular.module("CarmenSandiego")
             ]
         },
         "paisesVisitados": ["Egipto", "Argentina"],
-        "paisesFallidos": ["Uruguay", "Peru"]
+        "paisesFallidos": ["Uruguay", "Peru", "China"]
+    }
+
+    this.changeVillano = function() {
+        alert("id: "+this.villanoSeleccionado);
+    }
+    this.getPista = function(lugar) {
+        //getPista
+        this.pista = "Pista de: " + lugar;
+        this.lugarSeleccionado = lugar;
+        $('#modalLugar').modal('open');
+    }
+
+    this.emitirOrdenArresto = function() {
+        //post villano.id
+        alert("id: "+ this.villanoSeleccionado);
+
     }
 });
