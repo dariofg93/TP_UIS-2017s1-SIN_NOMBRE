71b64b09f97b2427e303f5d8ac5cc1ce11224624
Nico <nicoruiz182@gmail.com>
2017-05-28 22:14:34 -0300

    NR - Por fin pude enviar un post con body y recibirlo en el webservice

--- Web_CarmenSandiegoAngular/js/resolverMisterio-controller.js
+++ Web_CarmenSandiegoAngular/js/resolverMisterio-controller.js
@@ -1,44 +1,59 @@
 angular.module("CarmenSandiego")
 
 .controller("ResolverMisterioController", function($scope, $http) {
-    //spinner y ocultar container
-    $(".container").hide();
-    $(".spinner").show();
-
+    var url = "http://localhost:9000";
+    var header = {'Content-Type': 'application/x-www-form-urlencoded'};
     $scope.title = "Resolver Misterio";
     $scope.viewData = {};
-    $scope.lugarSeleccionado;
-    $scope.pista; 
-    $scope.villanos;
-    $scope.villanoSeleccionado;
 
     //Post iniciar juego
-    $http.post("http://localhost:9000/iniciarJuego").then(function(response, status) {
+    $http.post(url + "/iniciarJuego").then(function(response, status) {
         $scope.caso = response.data;
+        $scope.idCaso = response.data.id;
     });
 
     //Get villanos
-    $http.get("http://localhost:9000/villanos").then(function(response, status) {
-        $scope.villanos = response;
+    $http.get(url + "/villanos").then(function(response, status) {
+        $scope.villanos = response.data;
+        $scope.villanoSeleccionado = response.data[0].id;
     });
 
-    $(".container").show();
-    $(".spinner").hide();
+    //Post viajar
+    $scope.viajar = function() {
+        $http.post(url + "/viajar", {}).then(function(response, status) {
+            $scope.caso = response.data;
+            $scope.idCaso = response.data.id;
+        });
+    }
 
+    //Get pista
     $scope.getPista = function(lugar) {
-        //getPista
-        $scope.pista = "Pista de: " + lugar;
-        $scope.lugarSeleccionado = lugar;
+        $http.get(url + "/pistaDelLugar/"+$scope.idCaso+"/"+lugar).then(function(response, status) {
+            $scope.lugarSeleccionado = response.data;
+        });
         $('#modalLugar').modal('show');
     }
 
     $scope.emitirOrdenArresto = function() {
-        //post villano.id
-        alert("id: "+ $scope.villanoSeleccionado);
-    }
+        $http({
+            method: 'POST',
+            url: url + "/emitirOrden",
+            data: {"villanoId": $scope.villanoSeleccionado, "casoId": $scope.idCaso},
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function(response, status) {
+            $scope.ordenEmitida = response.data;
+            alert(response.data);
+        },
+        function(error) {
+            alert("error: " + error.data);
+        });
 
-    $scope.viajar = function() {
-        //post pais.id
-        alert("Pais: "+ $scope.paisSeleccionado.nombre);
+
+        /*$http.post(url + "/emitirOrden", {villanoId: $scope.villanoSeleccionado, casoId: $scope.idCaso}).then(function(response, status) {
+            $scope.ordenEmitida = response.data;
+        },
+        function(error) {
+            alert("error: " + error.data);
+        });*/
     }
 });
