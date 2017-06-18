4d3590ac4093adf7d5e15eca4d201a4be3842955
Nico <nicoruiz182@gmail.com>
2017-06-18 03:07:46 -0300

    Agregado fragments para cuando se cmabian de vistas

---
+++ Android_CarmenSanDiego/CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/MainActivity.java
@@ -0,0 +1,52 @@
+package com.uis.carmensandiego.carmensandiego;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        fragment = new FavouriteFragment();
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.content, fragment).commit();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.ordenArresto:
+                            fragment = new FavouriteFragment();
+                            break;
+                        case R.id.viajar:
+                            fragment = new VideoFragment();
+                            break;
+                        case R.id.pistas:
+                            fragment = new MusicFragment();
+                            break;
+                    }
+                    final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                    transaction.replace(R.id.content, fragment).commit();
+                    return true;
+                }
+            });
+}
+}
