a200428e2405b4e67f1bd3b8942d6985bf98aa32
cesar <perez.cesaar@gmail.com>
2017-06-26 13:41:51 -0300

    VISTA DE PISTAS

--- Android_CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/OrdenArrestoFragment.java
+++ Android_CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/OrdenArrestoFragment.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import retrofit2.http.POST;
+//import retrofit2.http.POST;
 
 public class OrdenArrestoFragment extends Fragment {
 
