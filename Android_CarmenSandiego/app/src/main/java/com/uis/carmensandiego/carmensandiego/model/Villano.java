2e18cd4e250fbd024762d5e1aeec489ec21d2e3d
cesar <perez.cesaar@gmail.com>
2017-06-26 16:28:24 -0300

    algunos dtos

---
+++ Android_CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/model/Villano.java
@@ -0,0 +1,30 @@
+package com.uis.carmensandiego.carmensandiego.model;
+
+public class Villano {
+
+    private String nombre;
+
+
+    private int id;
+
+	public Villano(String unNombre, int unId){
+        this.nombre = unNombre;
+        this.id = unId;//nose si es necesario
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
