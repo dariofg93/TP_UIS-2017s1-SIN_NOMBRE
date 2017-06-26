cc1d7e7cb942648d36701ee5ca0b24c622557b09
dariofg93 <dariofg93@gmail.com>
2017-06-26 16:46:55 -0300

    servicios con retrofit

---
+++ Android_CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/service/CarmenSanDiegoService.java
@@ -0,0 +1,32 @@
+package com.uis.carmensandiego.carmensandiego.service;
+
+import com.uis.carmensandiego.carmensandiego.bodies.EmitirOrdenBody;
+import com.uis.carmensandiego.carmensandiego.bodies.ViajarBody;
+import com.uis.carmensandiego.carmensandiego.model.Caso;
+import com.uis.carmensandiego.carmensandiego.model.Pista;
+import com.uis.carmensandiego.carmensandiego.model.Villano;
+
+import java.util.List;
+import retrofit.Callback;
+import retrofit.http.Body;
+import retrofit.http.GET;
+import retrofit.http.POST;
+
+public interface CarmenSanDiegoService {
+    @POST("/iniciarJuego")
+    void iniciarJuego(Callback<Caso> callback);
+
+    @POST("/emitirOrden")
+    void emitirOrden(@Body EmitirOrdenBody body, Callback<String> callback);
+
+    @GET("/pistasDelLugar/{casoId}/{nombreLugar}")
+    void getLibro(@retrofit.http.Path("LibroId") String id,
+                  @retrofit.http.Path("nombreLugar") String lugar,
+                  Callback<Pista> callback);
+
+    @POST("/viajar")
+    void iniciarJuego(@Body ViajarBody body, Callback<Caso> callback);
+
+    @GET("/villanos")
+    void getVillanos(Callback<List<Villano>> callback);
+}
