1660c6702c1bfcc81c05c3bb20f38be65e0cf124
dariofg93 <dariofg93@gmail.com>
2017-06-26 22:12:23 -0300

    intento fallido de listar los paises pegandole con de todo al server

--- Android_CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/MainActivity.java
+++ Android_CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                             break;
                         case R.id.pistas:
                             fragment = new PistasFragment();
+                            ((PistasFragment)fragment).obtenerLugares();
                             break;
                     }
                     final FragmentTransaction transaction = fragmentManager.beginTransaction();
--- Android_CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/PistasFragment.java
+++ Android_CarmenSandiego/app/src/main/java/com/uis/carmensandiego/carmensandiego/PistasFragment.java
@@ -6,13 +6,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.uis.carmensandiego.carmensandiego.adapter.LugaresAdapter;
 import com.uis.carmensandiego.carmensandiego.model.Caso;
 import com.uis.carmensandiego.carmensandiego.service.CarmenSanDiegoService;
 import com.uis.carmensandiego.carmensandiego.service.Connection;
-
-import java.util.List;
 
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -20,23 +19,25 @@ import retrofit.client.Response;
 
 public class PistasFragment extends Fragment {
 
-    public PistasFragment() {
-    }
+    private ListView lvLugares;
+    private CarmenSanDiegoService carmenSanDiegoService = Connection.getInstance();
+
+    public PistasFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        inflater.inflate(R.layout.fragment_pistas, container, false);
+        lvLugares = (ListView) container.findViewById(R.id.listLugares);
 
-        obtenerLugares();
-        return null;
+        return inflater.inflate(R.layout.fragment_pistas, container, false); // <-- AQUIII
     }
 
-    private void obtenerLugares() {
-        CarmenSanDiegoService carmenSanDiegoService = Connection.getInstance();
+    public void obtenerLugares() {
+
         carmenSanDiegoService.iniciarJuego(new Callback<Caso>() {
             @Override
             public void success(Caso caso, Response response) {
-                agregarLibros(caso.getPais().getLugares());
+                LugaresAdapter adapter = new LugaresAdapter(getActivity(),caso.getPais().getLugares());
+                lvLugares.setAdapter(adapter);
             }
 
             @Override
@@ -45,10 +46,6 @@ public class PistasFragment extends Fragment {
                 error.printStackTrace();
             }
         });
-    }
-
-    private void agregarLibros(List<String> lugares) {
-        //setListAdapter(new LugaresAdapter(getActivity(),lugares));
     }
 }
 
